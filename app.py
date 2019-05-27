c8cf0ab587c2b6844401b6e5d0205f80b9d8b1ea
Ricardo1921 <ricardojorgexavier2003@gmail.com>
2019-05-27 16:30:36 +0100

    footer

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ def index():
 def login():
     return render_template('login.html')
 
-@app.route('/registo')
-def registo():
-    return render_template('registo.html')
+@app.route('/Registo')
+def Registo():
+    return render_template('Registo.html')
+
 
 @app.route('/V')
 def V():
@@ -26,7 +27,9 @@ def C():
 def F():
     return render_template('F.html')
 
-
+@app.route('/Galo')
+def Galo():
+    return render_template('Galo.html')
 
 
 if __name__ == '__main__':
